a69da6001d4b899d8fda9f936ef54ea740988a1a
adaking1 <137830553+adaking1@users.noreply.github.com>
2023-09-21 13:18:57 -0400

    added comments and removed unneeded code

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
-// TODO: Include packages needed for this application
 const fs = require("fs").promises;
 const inquirer = require("inquirer");
 const generator = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-const questions = [{name: "title", message: "What is your project's title?"}, {name: "description", message: "Enter a description of your project"}, 
-                    {name: "installation", message: "Please provide installation instructions"}, {name: "usage", message: "Please provide usage information"}, 
-                    {name: "contributing", message: "Please provide guidelines for contributing to this project"}, {name: "tests", message: "Please include test instructions"}, 
-                    {name: "license", message: "Please select a license for this project"}, {name: "github", message: "What is your GitHub username?"}, 
-                    {name: "email", message: "What is your email address?"}];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, generator.markdown(data));
-}
-
-// TODO: Create a function to initialize app
+// this function starts the inquirer prompts 
+// they are called using recursion since all but one of the questions are formatted the same 
 const data = {};
 let index = 0;
 function init() {
+    // this is an array of objects, each with a name and message for inquirer to prompt
+    // it's inside the function because it is only referenced here
+    const questions = [{name: "title", message: "What is your project's title?"}, {name: "description", message: "Enter a description of your project"}, 
+            {name: "installation", message: "Please provide installation instructions"}, {name: "usage", message: "Please provide usage information"}, 
+            {name: "contributing", message: "Please provide guidelines for contributing to this project"}, {name: "tests", message: "Please include test instructions"}, 
+            {name: "credits", message: "Please credit the author(s) of this code and add any citations if needed"}, {name: "license", message: "Please select a license for this project"}, 
+            {name: "github", message: "What is your GitHub username?"}, {name: "email", message: "What is your email address?"}];
     if (questions[index].name !== "license") {    
     inquirer.prompt([
         {
@@ -38,6 +33,7 @@ function init() {
                 return writeToFile("newREADME.md", data);
             }
         })
+        .catch((err) => console.error(err));
     }
     else{
         inquirer.prompt([
@@ -54,8 +50,11 @@ function init() {
             index++
             init();
         })
+        .catch((err) => console.error(err));
     }
 }
 
-// Function call to initialize app
+// This function call initializes the app when "node index.js" is entered in the terminal
 init();
+
+// make demo video and use that for readme
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,74 +1,57 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+// This function returns the link for the selected license's badge
+// This function gets called in the generateMarkdown function return statement
+// no break statements needed because the function gets returned in every case
+// no default case needed since every option in the choices list is covered in a case
 function renderLicenseBadge(license) {
     switch (license) {
       case "GNU AGPLv3":
         return "![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)";
-        break;
       case "GNU GPLv3":
         return "![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)";
-        break;
       case "GNU LGPLv3":
         return "![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)";
-        break
       case "Mozilla Public License 2.0":
         return "![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)";
-        break;
       case "Apache License 2.0":
         return "![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)";
-        break
       case "MIT License":
         return "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)";
-        break;
       case "Boost Software License 1.0":
         return "![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)";
-        break;
       case "The Unlicense":
         return "![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)";
-        break;
       case "None":
         return "";
-        break;
     }
 }
   
-  // TODO: Create a function that returns the license link
-  // If there is no license, return an empty string
+// This function returns the link for the selected license's website
+// This gets called in the return statement for the generateMarkdown function 
 function renderLicenseLink(license) {
     const text = "For more information about this license, click the following link:"
     switch (license) {
     case "GNU AGPLv3":
       return `${text} https://www.gnu.org/licenses/agpl-3.0`;
-      break;
     case "GNU GPLv3":
       return `${text} https://www.gnu.org/licenses/gpl-3.0`;
-      break;
     case "GNU LGPLv3":
       return `${text} https://www.gnu.org/licenses/lgpl-3.0`;
-      break
     case "Mozilla Public License 2.0":
       return `${text} https://opensource.org/licenses/MPL-2.0`;
-      break;
     case "Apache License 2.0":
       return `${text} https://opensource.org/licenses/Apache-2.0`;
-      break
     case "MIT License":
       return `${text} https://opensource.org/licenses/MIT`;
-      break;
     case "Boost Software License 1.0":
       return `${text} https://www.boost.org/LICENSE_1_0.txt`;
-      break;
     case "The Unlicense":
       return `${text} http://unlicense.org/`;
-      break;
     case "None":
       return "";
-      break;
   }
 }
   
-  // TODO: Create a function that returns the license section of README
-  // If there is no license, return an empty string
+  // This function creates the text for the license section of the new readme
 function renderLicenseSection(license) {
     if (license === "None") {
         return "This project is not protected under any license."
@@ -77,25 +60,22 @@ function renderLicenseSection(license) {
         return `This project is protected under ${license}`
     }
 }
-  // TODO: Create a function to generate markdown for README
+
+  // This function returns the markdown template for the readme file
+  // This will be exported to the index.js file to be used with the inquirer prompt answers
 function generateMarkdown(data) {
     return `${renderLicenseBadge(data.license)}
 # ${data.title}
 
 ## Table of Contents
-[Description](#Description)
-
-[Installation](#Installation)
-
-[Usage](#Usage)
-
-[Contributing](#Contributing)
-
-[Tests](#Tests)
-
-[License](#License)
-
-[Questions](#Questions)
+-[Description](#description)  
+-[Installation](#installation)  
+-[Usage](#usage)  
+-[Contributing](#contributing)  
+-[Tests](#tests)  
+-[Credits](#credits)  
+-[License](#license)  
+-[Questions](#questions)
 
 ## Description
 ${data.description}
@@ -112,20 +92,24 @@ ${data.contributing}
 ## Tests
 ${data.tests}
 
+## Credits
+${data.credits}
+
 ## License
 ${renderLicenseSection(data.license)}
 
 ${renderLicenseLink(data.license)}
 
 ## Questions
+To view more of my work, view my GitHub profile: [${data.github}](https://github.com/${data.github})
 
-GitHub: [${data.github}](https://github.com/${data.github})
-
-Email: ${data.email}
+If you have any questions about this project please email me here: ${data.email}
 
 `;
 }
   
+// This object is used to store the information that can be read from this file
+// It is storing the function generateMarkdown as an object method to be exported to another file
 module.exports = {
 markdown: generateMarkdown
 };
